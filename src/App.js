8574fcc18a844cfa9c1df2d2651b8c2543f30cb4
mayanktiwari47 <43770764+mayanktiwari47@users.noreply.github.com>
2018-10-26 14:29:05 +0530

    update

--- src/App.js
+++ src/App.js
@@ -15,10 +15,11 @@ class App extends Component {
       <HashRouter>
         <Switch>
           <Route exact path="/login" name="Login Page" component={Login} />
+          <Route exact path="/" name="Login Page" component={Login} />
           <Route exact path="/register" name="Register Page" component={Register} />
           <Route exact path="/404" name="Page 404" component={Page404} />
           <Route exact path="/500" name="Page 500" component={Page500} />
-          <Route path="/" name="Home" component={DefaultLayout} />
+          <Route  path="/Dashboard" name="Home" component={DefaultLayout} />
         </Switch>
       </HashRouter>
     );
