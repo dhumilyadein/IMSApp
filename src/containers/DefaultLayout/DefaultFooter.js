74afddc9332bc2f98c8988c435b4391a8438e3a7
mayanktiwari47 <43770764+mayanktiwari47@users.noreply.github.com>
2018-11-02 11:02:04 +0530

    update

--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -16,7 +16,7 @@ class DefaultFooter extends Component {
     return (
       <React.Fragment>
         <span><a href="https://coreui.io">IMS</a> &copy; 2018 MAK</span>
-        <span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
+        <span className="ml-auto">Powered by </span>
       </React.Fragment>
     );
   }
