6cfff9945a859cb3f8a2b8b82c4306832f1bb93b
mayanktiwari47 <43770764+mayanktiwari47@users.noreply.github.com>
2019-04-24 05:59:20 +0530

    update

--- server/src/admin/leaves.js
+++ server/src/admin/leaves.js
@@ -219,22 +219,26 @@ if(req.body.carryForward)
 {var recordsUpdated=0;
 for(var i=0;i<req.body.selectedEmp.length;i++)
 
-{  await EmpLeaveStatus.findOne({empName:req.body.selectedEmp[i].label
+{  var empDataFound=null;
+  await EmpLeaveStatus.findOne({empName:req.body.selectedEmp[i].label.toLowerCase()
   //leaveDetails: {$elemMatch: {leaveType:req.body.leaveType}}
    })
-  .then(data => {
+  .then (data =>  {
     if(data!=null)
+   { console.log("Emp Leave Data Found: "+JSON.stringify(data));
+empDataFound=data;}});
 
+
+if(empDataFound)
     {
-        console.log("Emp Leave Data Found: "+JSON.stringify(data));
           var leaveTypeFound=false;
-          for(var j=0;j <data.leaveDetails.length;j++)
-          if(data.leaveDetails[j].leaveType===req.body.leaveType)
+          for(var j=0;j <empDataFound.leaveDetails.length;j++)
+          if(empDataFound.leaveDetails[j].leaveType===req.body.leaveType)
           {  leaveTypeFound=true;
 
-            if((parseInt(data.leaveDetails[j].total)+parseInt(data.leaveDetails[j].remaining))>req.body.maxLeaveCount)
-{ console.log("In IF "+data.empName);
-            EmpLeaveStatus.findOneAndUpdate({empName:data.empName,
+            if((parseInt(empDataFound.leaveDetails[j].total)+parseInt(empDataFound.leaveDetails[j].remaining))>req.body.maxLeaveCount)
+{ console.log("MAX Count crossed "+empDataFound.empName);
+          await  EmpLeaveStatus.findOneAndUpdate({empName:req.body.selectedEmp[i].label.toLowerCase(),
               'leaveDetails.leaveType': req.body.leaveType}, {'$set': {
                 'leaveDetails.$.total': req.body.maxLeaveCount,
                 'leaveDetails.$.used':0,
@@ -243,38 +247,44 @@ for(var i=0;i<req.body.selectedEmp.length;i++)
                 'leaveDetails.$.maxLeaveCount':req.body.maxLeaveCount,
 
 
-            }},{new: true}).then(data=>{recordsUpdated++;})}
+            }},{new: true}).then(data=>{
+              console.log("ESLE data: "+ JSON.stringify(data));
+
+              recordsUpdated++;})}
             else
-{ console.log("In ELSE");
-           EmpLeaveStatus.findOneAndUpdate({empName:data.empName,
+{ console.log("UNDER MAX COUNT");
+         await   EmpLeaveStatus.findOneAndUpdate({empName:empDataFound.empName,
             'leaveDetails.leaveType': req.body.leaveType}, {'$set': {
-              'leaveDetails.$.total': req.body.leaveCount+data.leaveDetails[j].remaining,
+              'leaveDetails.$.total': req.body.leaveCount+empDataFound.leaveDetails[j].remaining,
               'leaveDetails.$.used':0,
-              'leaveDetails.$.remaining': req.body.leaveCount+data.leaveDetails[j].remaining,
+              'leaveDetails.$.remaining': req.body.leaveCount+empDataFound.leaveDetails[j].remaining,
               'leaveDetails.$.carryForward':req.body.carryForward,
               'leaveDetails.$.maxLeaveCount':req.body.maxLeaveCount,
 
-          }}, {new: true}).then(data=>{recordsUpdated++;})}
-
+          }}, {new: true}).then(data=>{
+             console.log("ESLE data: "+ JSON.stringify(data));
+            recordsUpdated++;})}
 
 
           }
 
+
           if(!leaveTypeFound)
-          EmpLeaveStatus.findOneAndUpdate(
+       await   EmpLeaveStatus.findOneAndUpdate(
             { empName:req.body.selectedEmp[i].label },
             { $push: { leaveDetails: {"leaveType":req.body.leaveType,"total": parseInt(req.body.leaveCount)+parseInt(req.body.CFLC),"used":0,remaining:  parseInt(req.body.leaveCount)+parseInt(req.body.CFLC),  carryForward:req.body.carryForward,
             maxLeaveCount:req.body.maxLeaveCount} } }, {new: true})
-            .then(LeaveDetailsData2 => {
- console.log("LeaveDetailsData2 "+JSON.stringify(LeaveDetailsData2))
-              if(req.body.maxLeaveCount<LeaveDetailsData2.leaveDetails.total)
-              EmpLeaveStatus.findOneAndUpdate({empName:req.body.selectedEmp[i].label,
+            .then(data => { if(data!=null){
+
+ console.log("data "+JSON.stringify(data));}
+                for(var k=0;k<data.leaveDetails.length;k++)
+              if(data.leaveDetails[k].leaveType===req.body.leaveType&& req.body.maxLeaveCount<data.leaveDetails[k].total)
+              EmpLeaveStatus.findOneAndUpdate({empName:req.body.selectedEmp[i].label.toLowerCase(),
                 'leaveDetails.leaveType': req.body.leaveType}, {'$set': {
-                  'leaveDetails.$.total': LeaveDetailsData2.leaveDetails.maxLeaveCount,
-                  'leaveDetails.$.used':0,
-                  'leaveDetails.$.remaining': LeaveDetailsData2.leaveDetails.maxLeaveCount,
-                  'leaveDetails.$.carryForward':req.body.carryForward,
-                  'leaveDetails.$.maxLeaveCount':LeaveDetailsData2.leaveDetails.maxLeaveCount,
+                  'leaveDetails.$.total': data.leaveDetails[k].maxLeaveCount,
+                   'leaveDetails.$.remaining': data.leaveDetails[k].maxLeaveCount,
+
+
 
 
 
